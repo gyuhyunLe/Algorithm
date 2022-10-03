612ffd9aea2bedc7d1127a9f6318efa34ab2f998
gyuhyun <dlrbgus95@gmail.com>
2022-10-03 16:44:07 +0900

    나머지가 1이 되는 수 찾기

--- src/programmers/week3/week3_3.java
+++ src/programmers/week3/week3_3.java
@@ -1,4 +1,18 @@
 package src.programmers.week3;
+/*
+문제 - 자연수 n이 매개변수로 주어집니다. n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요.
 
-public class week3_3 {
+해결 - 입력받은 수를 2로 나눠 나머지가 1인 수를 리턴한다.
+
+출처 - https://school.programmers.co.kr/learn/courses/30/lessons/87389
+ */
+class week3_3 {
+    public int solution(int n) {
+        for(int i=2; i<n; i++) {
+            if(n%i == 1) {
+                return i;
+            }
+        }
+        return 0;
+    }
 }
