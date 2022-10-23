f331ff50947fd68a130b21d8bac02e3ff4c909d3
gyuhyun <dlrbgus95@gmail.com>
2022-10-23 16:55:15 +0900

    큰 수 만들기

---
+++ src/programmers/week6/week6_1.java
@@ -0,0 +1,30 @@
+package src.programmers.week6;
+/*
+문제 -   어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다.
+        예를 들어, 숫자 1924에서 수 두 개를 제거하면 [19, 12, 14, 92, 94, 24] 를 만들 수 있습니다. 이 중 가장 큰 숫자는 94 입니다.
+        문자열 형식으로 숫자 number와 제거할 수의 개수 k가 solution 함수의 매개변수로 주어집니다. number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 문자열 형태로 return 하도록 solution 함수를 완성하세요.
+
+해결 - 1. for문의 범위는 탐색해야하는 문자의 시작(index)부터, 앞으로 이어붙혀야할 문자의 길이 -1
+      2. index에는 가장 큰수 다음 index를 넣어준다.
+      3.
+
+출처 - https://school.programmers.co.kr/learn/courses/30/lessons/42883
+*/
+class week6_1 {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        int max = 0;
+        for(int i=0; i<number.length() - k; i++) {
+            max = 0;
+            for(int j = index; j<= k+i; j++) {
+                if(max < number.charAt(j)-'0') {  //int형 변환
+                    max = number.charAt(j)-'0';
+                    index = j+1;
+                }
+            }
+            sb.append(max);
+        }
+        return sb.toString();
+    }
+}
