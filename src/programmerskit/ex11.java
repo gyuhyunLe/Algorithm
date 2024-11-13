4a46bad501e77b3daa3704ec02177ff8698774c6
gyuhyunLe <dlrbgus95@gmail.com>
2024-11-13 21:16:02 +0900

    더 맵게

--- src/programmerskit/ex11.java
+++ src/programmerskit/ex11.java
@@ -8,7 +8,7 @@ import java.util.PriorityQueue;
 public class ex11 {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>();//오름차순 정렬
         for(int i=0; i<scoville.length; i++) {
             queue.add(scoville[i]);
         }
